2b95f8637ef8204fdecd1b512542ea9b96d984fd
amandaamarques <amandaalvaro.marques@estudante.ufjf.br>
2023-11-22 12:41:54 -0300

    Criei JoaoEMaria.js em src e defini variaveis

---
+++ src/joaoEMaria.js
@@ -0,0 +1,10 @@
+//define duas variáveis
+let joao = 1;
+let maria = 2;
+
+// funções deJoaoParaMaria e deMariaParaJoao transferem todas as maçãs de uma pessoa para a outra
+function deJoaoParaMaria(){
+    maria += joao;
+    joao = 0;
+}
+
